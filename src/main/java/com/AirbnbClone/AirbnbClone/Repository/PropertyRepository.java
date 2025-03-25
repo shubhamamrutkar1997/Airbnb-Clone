960235fd3e0d55650561b4a470e6f64beae22233
Shubham.R <sr.amrutkar4@gmail.com>
2025-03-25 11:56:07 +0530

    repo classes/interfaces done

--- src/main/java/com/AirbnbClone/AirbnbClone/Repository/PropertyRepository.java
+++ src/main/java/com/AirbnbClone/AirbnbClone/Repository/PropertyRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PropertyRepository extends JpaRepository<Property, Long> {
+public interface PropertyRepository extends JpaRepository<Property, Long>{
 }
